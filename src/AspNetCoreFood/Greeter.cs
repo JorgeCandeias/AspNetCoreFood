4ca234f6dc2eb31b78cef028aa3cc0fe5c1eb6ea
Jorge Candeias <candeias@fastmail.fm>
2018-11-20 17:40:40 +0000

    Added IConfiguration dependency to Greeter class.

--- src/AspNetCoreFood/Greeter.cs
+++ src/AspNetCoreFood/Greeter.cs
@@ -1,10 +1,19 @@
-﻿namespace AspNetCoreFood
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AspNetCoreFood
 {
     public class Greeter : IGreeter
     {
+        private IConfiguration _config;
+
+        public Greeter(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public string SetMessageOfTheDay()
         {
-            return "Greetings!";
+            return _config["Greeting"];
         }
     }
 }
