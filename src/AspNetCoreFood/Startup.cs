b6a1078ab82e7e5c4acf9923ddfca81305ec309a
Jorge Candeias <candeias@fastmail.fm>
2018-11-20 17:59:26 +0000

    Added custom middleware.

--- src/AspNetCoreFood/Startup.cs
+++ src/AspNetCoreFood/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreFood
 {
@@ -16,12 +17,30 @@ namespace AspNetCoreFood
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration cfg, IGreeter greeter)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration cfg, IGreeter greeter, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(next =>
+            {
+                return async context =>
+                {
+                    logger.LogInformation("Request incoming");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit!");
+                        logger.LogInformation("Request handled");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Request outgoing");
+                    }
+                };
+            });
 
             app.UseWelcomePage(new WelcomePageOptions
             {
